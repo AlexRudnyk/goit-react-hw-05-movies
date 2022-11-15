3296cc3ba059ecd81ea79a6cd3e131e2fb2356fd
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-11-15 21:47:41 +0200

    Implement Box and Theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </>
+  );
 };
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+import { Box } from 'components/Box';
+
+export const Layout = () => {
+  return (
+    <Box display="flex" px={5} py={4}>
+      <div>Home</div>
+      <div>Movies</div>
+    </Box>
+  );
+};
