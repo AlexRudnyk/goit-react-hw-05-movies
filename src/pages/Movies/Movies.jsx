d6072d825703e0fbc931e60ba8831b0bb1c976b4
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-11-16 19:46:40 +0200

    Makes Formik and yup validation

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,5 +1,31 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  inputValue: yup
+    .string()
+    .min(2, 'Too Short!')
+    .max(70, 'Too Long!')
+    .required('Required'),
+});
+
 const Movies = () => {
-  return <></>;
+  const handleSubmit = (values, { resetForm }) => {
+    resetForm();
+  };
+  return (
+    <Formik
+      initialValues={{ inputValue: '' }}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <Field type="text" name="inputValue" />
+        <button type="submit">Search</button>
+        <ErrorMessage name="inputValue" component="div" />
+      </Form>
+    </Formik>
+  );
 };
 
 export default Movies;
